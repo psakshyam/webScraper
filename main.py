44b464e7053830b61ef15f7c198a65d4de69cb66
Sakshyam Pokharel <sp2297@princeton.edu>
2025-06-18 13:46:44 -0400

    cleaning the text

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from scrape import scrape_website, split_text, clean_body, extract_data
 
 st.title("Web Scraper")
 url = st.text_input("Enter the URL to scrape:")
@@ -8,5 +9,17 @@ if st.button("Scrape"):
         st.write(f"Scraping data from: {url}")
         # Here you would add the logic to scrape the URL
         # For example, using requests and BeautifulSoup
+        result = scrape_website(url)
+
+        body_content = extract_data(result)
+        cleaned_content = clean_body(body_content)
+
+        st.session_state.cleaned_content = cleaned_content
+
+        with st.expander("View Cleaned Content"):
+            st.text_area("Cleaned Content", cleaned_content, height=300)
+        
+        text_chunks = split_text(cleaned_content)
     else:
         st.error("Please enter a valid URL.")
+
---
+++ scrape.py
@@ -0,0 +1,50 @@
+import selenium.webdriver as webdriver
+from selenium.webdriver.chrome.service import Service
+from bs4 import BeautifulSoup
+import time
+
+def scrape_website(website):
+    print("Lauching browser...")
+
+    chrome_driver_path = "./chromedriver"
+    options = webdriver.ChromeOptions()
+    driver = webdriver.Chrome(service=Service(chrome_driver_path), options=options)
+
+    try:
+        driver.get(website)
+        print(f"Scraping data from: {website}")
+        html = driver.page_source
+
+        time.sleep(10)  # Wait for the page to load completely
+
+        return html
+    except Exception as e:
+        print(f"An error occurred: {e}")
+    finally:
+        driver.quit()
+        print("Browser closed.")
+
+def extract_data(html):
+    # Placeholder for data extraction logic
+    # This function would parse the HTML and extract the required data
+    soup = BeautifulSoup(html, 'html.parser')
+    body_content = soup.body
+    if body_content:
+        return str (body_content)
+    else:
+        return ""
+    return html 
+
+def clean_body(body_content):
+    soup = BeautifulSoup(body_content, 'html.parser')
+    for script_or_style in soup(['script', 'style']):
+        script_or_style.extract()
+    
+    cleaned = soup.get_text(seperator='\n')
+    cleaned = "\n".join(line.strip() for line in cleaned.splitlines() if line.strip())
+
+    return cleaned
+
+def split_text(text, max_length=6000):
+    return [text[i:i + max_length] for i in range(0, len(text), max_length)]
+
